c856ec18424a6a8560ead764f369ed11666d2959
Jonas Bushart <jonas@bushart.org>
2012-12-10 13:42:42 +0100

    A very basic main class, but functional

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
-import org.bushart.freiesmagazin_contest_2012_12.model.*;
+import org.bushart.freiesmagazin_contest_2012_12.controller.Bot;
 
 public class Main {
-
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		World<FieldState> map = new World<FieldState> ("GAMEBOARDSTART 8,4\n.oooooo.\noo####oo\noo####oo\n.oooooo.");
-		
-		//TODO everything
-		//create new Bot
-		//give controlling to Bot
-		System.out.print("Hallo Welt\n");
+		Bot bot = new Bot ();
+		bot.run ();
 	}
 }
